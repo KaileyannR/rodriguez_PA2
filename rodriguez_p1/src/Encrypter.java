1f76a6ee16adbb2b2338bb9df9773e3a85de33d9
Kaileyann Rodriguez <60050415+KaileyannR@users.noreply.github.com>
2020-02-01 23:47:51 -0500

    Update Encrypter.java

--- rodriguez_p1/src/Encrypter.java
+++ rodriguez_p1/src/Encrypter.java
@@ -2,7 +2,35 @@
 // COP 3330
 // Programming Assignment 2
 
-import java.util.Scanner;
 public class Encrypter
 {
+	public int encrypt(int number)
+	{
+		// Turns each digit in number into singular digits.
+		int temp = 0;
+		int digit1 = (int)(number / 1000);
+		int digit2 = (int)((number / 100) - (digit1 * 10));
+		int digit3 = (int)((number / 10) - (digit1 * 100) - (digit2 * 10));
+		int digit3 = (int)((number) - (digit1 * 1000) - (digit2 * 100) - (digit3 * 10));
+
+		// Performs encryption conversion upon each digit.
+		digit1 = (digit1 + 7) % 10;
+		digit2 = (digit1 + 7) % 10;
+		digit3 = (digit1 + 7) % 10;
+		digit4 = (digit1 + 7) % 10;
+
+		// Swaps digit1 with digit3.
+		temp = digit1;
+		digit1 = digit3;
+		digit3 = temp;
+
+		// Swaps digit2 with digit4.
+		temp = digit2;
+		digit2 = digit4;
+		digit4 = temp;
+
+		// Makes calculation to place all digits into a four digit number.
+		number = ((digit1 * 1000) + (digit2 * 100) + (digit3 * 10) + digit4));
+		return number;
+	}
 }
