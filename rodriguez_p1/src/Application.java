773f84f378d84eb0303cc8e0f9d246e278b7f4d4
Kaileyann Rodriguez <60050415+KaileyannR@users.noreply.github.com>
2020-02-01 23:48:37 -0500

    Update Application.java

--- rodriguez_p1/src/Application.java
+++ rodriguez_p1/src/Application.java
@@ -5,4 +5,14 @@
 import java.util.Scanner;
 public class Application
 {
+	public static void main (String [] args)
+	{
+		Scanner digit = new Scanner(System.in);
+		System.out.print("Enter four digit integer: ");
+		int number = digit.newInt();
+		Encrypter encrypter = new Encrypter;
+		Decrypter decrypter = new Decrypter;
+		encrypter.encrypt();
+		decrypter.decrypt();
+	}
 }
